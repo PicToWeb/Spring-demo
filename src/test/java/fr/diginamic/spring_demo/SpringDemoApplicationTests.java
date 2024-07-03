3de8b1d19d1f30f568c36c83df33fb6ccfde79dd
PicToWeb <picronantoine34@gmail.com>
2024-07-03 10:59:23 +0200

    TP1-spring

---
+++ src/test/java/fr/diginamic/spring_demo/SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diginamic.spring_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
