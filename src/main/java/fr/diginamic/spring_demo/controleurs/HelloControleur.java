b64be6ab29de461ef7267a356c42bc5457c947ca
PicToWeb <picronantoine34@gmail.com>
2024-07-03 12:08:13 +0200

    TP2

--- src/main/java/fr/diginamic/spring_demo/controleurs/HelloControleur.java
+++ src/main/java/fr/diginamic/spring_demo/controleurs/HelloControleur.java
@@ -1,12 +1,18 @@
 package fr.diginamic.spring_demo.controleurs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.diginamic.spring_demo.services.HelloService;
+
 @RestController
 @RequestMapping("/saluer")
 public class HelloControleur {
+	
+	@Autowired
+	public HelloService service;
 	
 	@GetMapping("/hello")
 	public String direHello() {
@@ -15,7 +21,7 @@ public class HelloControleur {
 	
 	@GetMapping("/bonjour")
 	public String direBonjour() {
-		return "Bonjour";
+		return service.salutations();
 	}
 
 }
